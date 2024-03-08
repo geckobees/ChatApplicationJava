e7dd03c04e299853674ea9ed5f8925fd26186697
geckobees <98201133+geckobees@users.noreply.github.com>
2024-03-08 10:57:31 -0500

    started Server

--- src/Server.java
+++ src/Server.java
@@ -5,14 +5,14 @@ public class Server {
     public static void main(String[] args) {
         final int PORT = 12345;
 
-        try (ServerSocket serverSocket = new ServerSocket(12346)) {
+        try (
+                ServerSocket serverSocket = new ServerSocket(12346);
+                Socket clientSocket = serverSocket.accept();
+                ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream())
+        ) {
             System.out.println("Server started. Waiting for client connections...");
 
-            while (true) {
-                Socket clientSocket = serverSocket.accept();
-                ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
-                System.out.println("Client connected: " + clientSocket);
-            }
+            System.out.println("Client connected: " + clientSocket);
         } catch (IOException e) {
             e.printStackTrace();
         }
