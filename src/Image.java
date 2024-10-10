c6078de2dca9524eca5f060d3446a362f84555f0
Ruslan <98201133+geckobees@users.noreply.github.com>
2024-10-10 09:38:26 -0400

    new stuff

--- src/Image.java
+++ src/Image.java
@@ -1,9 +1,21 @@
 
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.awt.image.BufferedImage;
+
+
 public class Image {
 
     String link;
+    long Image;
 
     public Image(String link) {
         this.link = link;
+    }
+
+    public long createImage(){
+
+        return Image;
     }
 }
